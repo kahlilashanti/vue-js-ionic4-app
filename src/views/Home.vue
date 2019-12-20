c219e7df52fc844a604f58d93bce00ae75035697
seetsdev <bushongo@protonmail.com>
2019-12-19 16:48:09 -0800

    install of dependencies and initial build

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipCode App</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      Here's the App
+    </ion-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+    
+  }
+}
+</script>
